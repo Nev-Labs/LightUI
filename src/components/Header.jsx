10be9da1368c6449087f5749df8b40b665de1bda
never <nevernevernever69@protonmail.com>
2024-02-11 01:20:43 +0530

    added Header and output Alignment

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+const Header = () => {
+	return(
+		<div className="header">
+		<div className="bar">
+		<div className="logo">LightUI</div>
+		<div className="otherele">
+		<p>About Us</p>
+		<p>Contact Us</p>
+		</div>
+		</div>
+		</div>
+
+	)	
+
+}
+export default Header;
---
+++ src/components/Loading.jsx
@@ -0,0 +1,8 @@
+const Loading = () => {
+	return (
+		<div className="loader">
+		<div className="dot-elastic"></div>
+		</div>
+	)
+}
+export default Loading;
